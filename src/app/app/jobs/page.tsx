e9a00de28da6626f3cdc19c759ec4f8277f17520
RIMSHAALEEM <92934551+RIMSHAALEEM@users.noreply.github.com>
2024-10-01 12:22:23 +0500

    Add files via upload

---
+++ src/app/app/jobs/page.tsx
@@ -0,0 +1,24 @@
+import Header from "../components/header";
+export default function Jobs(){
+  return(
+    
+    <div className="parent">
+    <Header/>
+        <div className="child">
+          <h2>Jobs/Internships comming soon</h2>
+          
+          
+          <button>Employment</button>
+          </div>
+    
+          {/* div for imagess */}
+    
+          <div className="images">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3DkxVSB6SzyZglgkG_pX03x9GZm_XJxaD6Nw37IXjy_0V6KJ9Fb184zWtXUxKrwJLM8E&usqp=CAU" className="girl" />
+    <img src="" className="shape" />        
+    
+          </div>
+    </div>
+    
+      )
+    };
---
+++ src/app/app/page.tsx
@@ -0,0 +1,30 @@
+import Header from "./components/header"
+
+export default function Home(){
+  return(
+    
+
+<div className="parent">
+<Header/>
+    <div className="child">
+      <h2>I am Rimsha Aleem</h2>
+      <p>This is my official portfolio website to <br></br>showcase my skills and work experience.</p>
+      
+      <button>Download Resume</button>
+      </div>
+
+      {/* div for imagess */}
+
+      <div className="images">
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpIn6BGYr2jGAQ_7XJ4J4dkAcKWU_vm1WAZA&s" className="girl" />
+<img src="" className="shape" />        
+
+      </div>
+</div>
+
+  )
+};
+
+
+
+   
